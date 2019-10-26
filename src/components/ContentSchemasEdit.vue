a42f322a3b867cc8606227ed87c6eae3c3f364e3
limzykenneth <limzy.kenneth@gmail.com>
2019-10-26 20:53:17 +0100

    Fix import error

--- src/components/ContentSchemasEdit.vue
+++ src/components/ContentSchemasEdit.vue
@@ -38,7 +38,7 @@
 </template>
 
 <script>
-import cloneDeep from "lodash/lang";
+import cloneDeep from "lodash";
 import snakeCase from "snake-case";
 import SchemasEditField from "./ContentSchemasEditField.vue";
 
@@ -89,7 +89,7 @@ export default{
 
 			if(newName !== oldName){
 				const appSlug = snakeCase(name);
-				this.$set(this.definition, appSlug,cloneDeep(this.definition[snakeCase(oldName)]));
+				this.$set(this.definition, appSlug, cloneDeep(this.definition[snakeCase(oldName)]));
 				this.$set(this.definition[appSlug], "app_title", name);
 				this.$delete(this.definition, oldName);
 			}
